67a9d36b4667e9ef23042dd2332447bee7b05317
keisukehasegawa0918 <chiro9689@gmail.com>
2022-10-06 16:57:12 +0900

    課題再提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,34 +1,45 @@
 package practice;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class Chapter5 {
 
 	public static void main(String[] args) {
-		
+
 		LocalDate d1 = LocalDate.of(2021, 10, 21);
 		LocalDate d2 = LocalDate.of(2021, 9, 15);
 		LocalDate d3 = LocalDate.of(2021, 12, 4);
 		LocalDate d4 = LocalDate.of(2021, 8, 10);
 		LocalDate d5 = LocalDate.of(2021, 11, 9);
-		
-		Map<LocalDate, String> taskLists = new HashMap<LocalDate, String>();
-		taskLists.put(d1, "牛乳を買う。");
-		taskLists.put(d2, "〇〇社面談。");
-		taskLists.put(d3, "手帳を買う。");
-		taskLists.put(d4, "散髪に行く。");
-		taskLists.put(d5, "スクールの課題を解く。");
-		
-		List<LocalDate> dList = new ArrayList<LocalDate>(taskLists.keySet());
-		dList.sort((s1, s2) -> s1.compareTo(s2));
-		for (LocalDate d : dList) {
-			System.out.println(d + ":" + taskLists.get(d));
+
+		List<Task> taskList = new LinkedList<Task>();
+		taskList.add(new Task(d1, "牛乳を買う。"));
+		taskList.add(new Task(d2, "〇〇社面談。"));
+		taskList.add(new Task(d3, "手帳を買う。"));
+		taskList.add(new Task(d4, "散髪に行く。"));
+		taskList.add(new Task(d5, "スクールの課題を解く。"));
+
+		Collections.sort(taskList, new Task());
+		for (Task task : taskList) {
+			System.out.println(task.getDate() + ":" + task.getTask());
 		}
-		
+
+//		Map<LocalDate, String> taskLists = new HashMap<LocalDate, String>();
+//		taskLists.put(d1, "牛乳を買う。");
+//		taskLists.put(d2, "〇〇社面談。");
+//		taskLists.put(d3, "手帳を買う。");
+//		taskLists.put(d4, "散髪に行く。");
+//		taskLists.put(d5, "スクールの課題を解く。");
+
+//		List<LocalDate> dList = new ArrayList<LocalDate>(taskList.keySet());
+//		dList.sort((s1, s2) -> s1.compareTo(s2));
+//		for (LocalDate d : dList) {
+//			System.out.println(d + ":" + taskList.get(d));
+//		}
+
 //		for (Map.Entry<LocalDate, String> task : taskLists.entrySet()) {
 //			System.out.println(task.getKey() + ":" + task.getValue());
 //		}
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,13 +1,22 @@
 package practice;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 
-public class Task {
+public class Task implements Comparator<Task> {
 
 	/** 日付 */
 	private LocalDate date;
 	/** タスク */
 	private String task;
+
+	public Task() {
+	}
+
+	public Task(LocalDate date, String task) {
+		this.date = date;
+		this.task = task;
+	}
 
 	public LocalDate getDate() {
 		return date;
@@ -23,6 +32,21 @@ public class Task {
 
 	public void setTask(String task) {
 		this.task = task;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [date=" + date + ", task=" + task + "]";
+	}
+
+	@Override
+	public int compare(Task o1, Task o2) {
+		if (o1.date.isBefore(o2.date)) {
+			return -1;
+		} else if (o1.date.isAfter(o2.date)) {
+			return 1;
+		}
+		return 0;
 	}
 
 }
